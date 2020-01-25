3e33883921ece6ae987a0b24f36eb32cb99302eb
zfhrp <coricozizi@gmail.com>
2020-01-26 05:15:36 +0900

    f

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-chmod 777 /srv/hakyll && \
-cd /srv/hakyll && \
+chmod 777 /github/workflow && \
+cd /github/workflow && \
 stack install --allow-different-user && \
 stack build --allow-defferent-user --fase && \
 stack exec site build
